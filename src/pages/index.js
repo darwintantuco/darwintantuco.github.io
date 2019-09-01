383035e19cbe5818f72922edec7b31d7bd6e0c85
darwin christopher tantuco <darwinxciii@yahoo.com>
2019-09-02 00:55:16 +0800

    Fix typo and react warning

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default () => (
   <div>
-    <p>I am a web developer fiddling with javascript, ruby and elixir.</p>
+    <p>{"I'm a web developer, fiddling with javascript, ruby and elixir."}</p>
     <p>I like building tools and have a soft spot for automation.</p>
-    <p>I also have passion for open-source projects.</p>
+    <p>{"I'm an avid open-source contributor as well."} </p>
   </div>
 )
