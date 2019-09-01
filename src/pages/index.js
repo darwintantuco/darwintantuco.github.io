212d85e8fd3e6bc565c4a4a3d0bf4248a0aa23d6
darwin christopher tantuco <darwinxciii@yahoo.com>
2019-09-02 00:34:31 +0800

    Initial index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,9 @@
 import React from 'react'
 
-export default () => <div>Hello world!</div>
+export default () => (
+  <div>
+    <p>I am a web developer fiddling with javascript, ruby and elixir.</p>
+    <p>I like building tools and have a soft spot for automation.</p>
+    <p>I also have passion for open-source projects.</p>
+  </div>
+)
