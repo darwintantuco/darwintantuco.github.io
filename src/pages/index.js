71eb5d6cdec28feff8cbc17f6c134d6b7dc05e73
darwin christopher tantuco <darwinxciii@yahoo.com>
2019-09-03 00:14:02 +0800

    Increase font-size

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,10 @@
 import React from 'react'
 
+import '../base.scss'
+import styles from './index.module.scss'
+
 const Index = () => (
-  <div>
+  <div className={styles.container}>
     <p>{"I'm a web developer, fiddling with javascript, ruby and elixir."}</p>
     <p>I like building tools and have a soft spot for automation.</p>
     <p>{"I'm an avid open-source contributor as well."} </p>
