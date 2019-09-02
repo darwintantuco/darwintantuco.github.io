48f5975abf072bf25c5fecb2842d0b89f6ab4f6d
darwin christopher tantuco <darwinxciii@yahoo.com>
2019-09-02 22:48:32 +0800

    Fix eslint offenses

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,11 @@
 import React from 'react'
 
-export default () => (
+const Index = () => (
   <div>
     <p>{"I'm a web developer, fiddling with javascript, ruby and elixir."}</p>
     <p>I like building tools and have a soft spot for automation.</p>
     <p>{"I'm an avid open-source contributor as well."} </p>
   </div>
 )
+
+export default Index
