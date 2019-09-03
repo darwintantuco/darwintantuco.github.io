8b0f17d42ac558e67a309ddc7a28d25f0c4d9018
darwin christopher tantuco <darwinxciii@yahoo.com>
2019-09-04 01:03:13 +0800

    Improve SEO

---
+++ src/components/SEO.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Helmet } from 'react-helmet'
+
+const SEO = () => (
+  <Helmet defer={false} defaultTitle={'Darwin Christopher Tantuco'}>
+    <html lang='en' />
+    <link rel='canonical' href={'https://dcrtantuco.com'} />
+    <meta name='docsearch:version' content='2.0' />
+    <meta
+      name='viewport'
+      content='width=device-width,initial-scale=1,shrink-to-fit=no,viewport-fit=cover'
+    />
+
+    <meta property='og:url' content={'https://dcrtantuco.com'} />
+    <meta property='og:type' content='website' />
+    <meta property='og:locale' content='en' />
+    <meta property='og:site_name' content={'Darwin Christopher Tantuco'} />
+
+    <meta name='twitter:card' content='summary' />
+    <meta name='twitter:site' content={'@dcrtantuco'} />
+  </Helmet>
+)
+
+export default SEO
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,16 +3,21 @@ import React from 'react'
 import '../base.scss'
 import styles from './index.module.scss'
 
-import SocialMediaLinks from '../components/SocialMediaLinks.js'
+import SEO from '../components/SEO'
+import SocialMediaLinks from '../components/SocialMediaLinks'
 
 const Index = () => (
-  <div className={styles.container}>
-    <p>{"I'm a web developer, fiddling with javascript, ruby and elixir."}</p>
-    <p>I like building tools and have a soft spot for automation.</p>
-    <p>{"I'm an avid open-source contributor as well."} </p>
-    <p>{"Let's connect."}</p>
-    <SocialMediaLinks />
-  </div>
+  <>
+    <SEO />
+
+    <div className={styles.container}>
+      <p>{"I'm a web developer, fiddling with javascript, ruby and elixir."}</p>
+      <p>I like building tools and have a soft spot for automation.</p>
+      <p>{"I'm an avid open-source contributor as well."} </p>
+      <p>{"Let's connect."}</p>
+      <SocialMediaLinks />
+    </div>
+  </>
 )
 
 export default Index
