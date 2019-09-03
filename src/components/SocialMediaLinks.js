0ffb8e677012e8c34537f81aca651a489610f256
darwin christopher tantuco <darwinxciii@yahoo.com>
2019-09-03 23:56:00 +0800

    Add social media links

---
+++ src/components/SocialMediaLinks.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+import styles from './SocialMediaLinks.module.scss'
+
+const SocialMediaLinks = () => (
+  <ul className={styles.container}>
+    <li>
+      <a href='mailto:dcrtantuco@gmail.com'>email</a>
+    </li>
+    <li>
+      <a href='https://github.com/dcrtantuco'>github</a>
+    </li>
+    <li>
+      <a href='https://linkedin.com/in/dcrtantuco'>linkedin</a>
+    </li>
+    <li>
+      <a href='https://twitter.com/dcrtantuco'>twitter</a>
+    </li>
+  </ul>
+)
+
+export default SocialMediaLinks
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,11 +3,15 @@ import React from 'react'
 import '../base.scss'
 import styles from './index.module.scss'
 
+import SocialMediaLinks from '../components/SocialMediaLinks.js'
+
 const Index = () => (
   <div className={styles.container}>
     <p>{"I'm a web developer, fiddling with javascript, ruby and elixir."}</p>
     <p>I like building tools and have a soft spot for automation.</p>
     <p>{"I'm an avid open-source contributor as well."} </p>
+    <p>{"Let's connect."}</p>
+    <SocialMediaLinks />
   </div>
 )
 
