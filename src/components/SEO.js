fdb93572d58f5fa49c726bba54a7ed6b489c2b39
darwin christopher tantuco <darwinxciii@yahoo.com>
2019-09-21 15:58:25 +0800

    Improve SEO

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -5,12 +5,12 @@ const SEO = () => (
   <Helmet defer={false} defaultTitle={'Darwin Christopher Tantuco'}>
     <html lang='en' />
     <link rel='canonical' href={'https://dcrtantuco.com'} />
-    <meta name='docsearch:version' content='2.0' />
     <meta
       name='viewport'
       content='width=device-width,initial-scale=1,shrink-to-fit=no,viewport-fit=cover'
     />
 
+    <meta property='og:title' content={'Darwin Christopher Tantuco'} />
     <meta property='og:url' content={'https://dcrtantuco.com'} />
     <meta property='og:type' content='website' />
     <meta property='og:locale' content='en' />
