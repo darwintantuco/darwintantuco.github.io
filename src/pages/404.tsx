57fc2bd2ca0698e4fef8dd66f937fb67583fbadf
darwin christopher tantuco <dcrtantuco@gmail.com>
2020-04-19 17:02:56 +0800

    Add simple 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
 
 import Layout from '../components/Layout'
+import styles from './404.module.scss'
 
-const Page404 = (): JSX.Element => (
-  <Layout>
-    <h1>404 </h1>
-  </Layout>
-)
+const Page404 = (): JSX.Element => {
+  return (
+    <Layout>
+      <h1 className={styles.code}>404 </h1>
+      <p className={styles.text}>Page not found </p>
+    </Layout>
+  )
+}
 
 export default Page404
